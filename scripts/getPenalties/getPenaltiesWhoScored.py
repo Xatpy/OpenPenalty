a93c17a53f7c6b142a9caefac357112af7fb24b0
Xatpy <jaime.chapinal@gmail.com>
2015-10-22 23:56:30 +0200

    Adding iterator with data-type=15

--- scripts/getPenalties/getPenaltiesWhoScored.py
+++ scripts/getPenalties/getPenaltiesWhoScored.py
@@ -1,168 +1,72 @@
-import urllib2, urlparse, string
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
-BASE_URL = "http://www.bdfutbol.com/es"
+driver = webdriver.Firefox()
 
-#import BeautifulSoup
-#from BeautifulSoup import BeautifulSoup
-from bs4 import BeautifulSoup
+driver.maximize_window()
+#driver.set_window_position(-110,-600)
+driver.maximize_window()
 
+driver.delete_all_cookies()
 
-import urllib2
-import threading
+print "ey"
 
-class MyHandler(urllib2.HTTPHandler):
-    def http_response(self, req, response):
-        print "url: %s" % (response.geturl(),)
-        print "info: %s" % (response.info(),)
-        for l in response:
-            print l
-        print "responseeeeeeee"
-        return response
+driver.get('http://www.whoscored.com/Matches/985501/Live')
 
-#o = urllib2.build_opener(MyHandler())
-#t = threading.Thread(target=o.open, args=('http://www.google.com/',))
-#t.start()
-#print "I'm asynchronous!"
+print "que pasa"
 
+links = driver.find_elements_by_partial_link_text('Match Commentary')
+for link in links:
+    print link.get_attribute("href")
+    link.click()
 
-def get_page2(url):
-    time.sleep(1.5)
-    print 'url:', url
-    try:
-    	print "vamos"
-        return urllib2.urlopen(url).read()
-    except urllib2.HTTPError as err:
-        print 'urllib2.HTTPError: \n\t url: %s \n\t err: %s' % (url, err)
+#element = WebDriverWait(driver, 500).until(
+#    EC.presence_of_element_located((By.CLASS_NAME,'wizards_mod'))
+#)
+#debugger('Click on wizards')
+#element.click()
 
-def getPage(theURL):
-  ## returns the HTML text of the URL
-	#req = urllib2.Request(theURL)
-	#thePage = urllib2.urlopen(req)
-	#time.sleep(5)
-	#theHTML = thePage.read()
+print "aqui estamos"
+total_pages = driver.find_element_by_class_name('total-pages')
+print total_pages.text
 
-	#print "theHTML " + theHTML
-
-	#theHTML = urllib2.urlopen(theURL)
-	#for line in theHTML:
-	#	print line
-	#print "hola"
-
-	#o = urllib2.build_opener(MyHandler())
-	#t = threading.Thread(target=o.open, args=(theURL,))
-	#t.start()
-
-	#soup = BeautifulSoup(urllib2.urlopen(theURL))
-	#title_ret = soup.title.string
-
+#spans = driver.find_elements_by_class_name('page-navigation-item')
+#for span in spans:
+	#print span
 	
-	#html = urllib2.urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
-	#bsObj = BeautifulSoup(html.read());
-	#print(bsObj.h1)
+test = driver.find_element_by_xpath("//*[@data-type=16]")
+if (test):
+	print "si"
+else:
+	print "no"
 
-	html = urllib2.urlopen(theURL)
-	soup = BeautifulSoup(html.read())
-	print(soup.h1)
-	title_ret = soup.title.string
-	#title_ret = ""
-	return title_ret
-	
-	#return theHTML
-
-def extractTitle(theLine):
-	## finds and returns contents of <title> tag
-	titleStart = theLine.find("<title>")
-	#print "ey " + theLine
-	if titleStart > -1: #make sure we found the tag, -1 will mean we did not
-		titleStart = titleStart + 7 #skip to end of tag
-		titleEnd = theLine.find("</title>")
-		theTitle = theLine[titleStart:titleEnd]
-		theTitle = theTitle.strip() # remove whitespace
-		print "vamossss"
+if 0:
+	elementA = WebDriverWait(driver, 5).until(
+		#EC.presence_of_element_located((By.CLASS_NAME, "//*[@data-type=16]"))
+		EC.presence_of_element_located((By.XPATH, "//*[@data-type=1500]"))
+		#elementA = driver.find_elements_by_xpath("//*[@data-type=16]")
+	)
+	if (elementA):
+		print "siiiii"
 	else:
-		theTitle = ""
-		print "joooo"
-		
-	return(theTitle)
+		print "nooooo"
+	#debugger('Click on wizards')
+	#elementA.click()
 
-def getNombrePartido(web):
-	for line in web:
-		if "</title>" in line:
-			return line
-	return ""
-
-
-def checkPenalty(web):
-	for line in web:
-		if 'class="GP"' in line:
-			return True
-	return False
-
-
-def processURL(url):
-	web = urllib2.urlopen(url)
-	web_aux = web
-	ret = ""
-	nombrePartido = getNombrePartido(web)
-	title = extractTitle
-	if checkPenalty(web):
-		ret = nombrePartido
-	return ret
-
-
-def extractLinksFromLine(line):
-    index = 0
-    while index < len(line):
-        index = line.find('/p/p.php?id=', index)
-        if index == -1:
-            break
-        link = line[index:index+17] # +5 characters from the id
-        link = BASE_URL + link
-        partidoConPenaltis = processURL(link)
-        if (partidoConPenaltis != ""):
-        	print partidoConPenaltis
-        	f.write(partidoConPenaltis)
-        index += 12 # +2 because len(''/p/p.php?id=') == 2
-
-
-def checkIfPenalty(lines):
-	for line in lines:
-		if 'class="GP"' in line:
-			return true
-	return false
-
-
-def processLeague():
-	#infile = open('liga2014-15.html', 'r')
-	#for line in infile:
-
-	urlLeague = 'http://www.bdfutbol.com/es/t/t2014-15.html'
-	league = urllib2.urlopen(urlLeague)
-
-	for line in league:
-		if "/p/p.php?id=" in line:
-			extractLinksFromLine(line)
-
-def processLeagueWhoScored():
-	urlBase = 'http://www.whoscored.com/Matches/'
-	#http://www.whoscored.com/Matches/862052/Live
-	start = 862052
-	end = 870000
-	cont = start
-	while ( cont <= end):
-		url = urlBase + str(cont) + "/Live"
-		print url
-		#processURL(url)
-		theTitle = getPage(url)
-		#theTitle = extractTitle(thePageHTML)
-		print theTitle
-		cont +=1
+list15 = driver.find_elements_by_xpath("//*[@data-type=15]")
+contador = 0
+for el in list15:
+	contador += 1
+	print str(contador) + " - " + el.text
+list16 = driver.find_elements_by_xpath("//*[@data-type=16]")
+list1500 = driver.find_elements_by_xpath("//*[@data-type=1500]")
 
 
 
-
-#Open file
-#f = open('output_t2014-15.txt', 'w')
-processLeagueWhoScored()
-#f.close()
+print "adios"
