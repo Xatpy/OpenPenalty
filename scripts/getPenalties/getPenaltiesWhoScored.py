47de7b583369b09bcb08d7b1134305f4a6d295ce
Xatpy <jaime.chapinal@gmail.com>
2015-10-26 00:26:24 +0100

    Iterating over urls.

--- scripts/getPenalties/getPenaltiesWhoScored.py
+++ scripts/getPenalties/getPenaltiesWhoScored.py
@@ -5,20 +5,27 @@ from selenium.webdriver.common.by import By
 import time
 
 SPANISH_TEAMS = ["Atletico Madrid", "Real Madrid", "Barcelona", "Valencia", "Sevilla",
-				 "Villareal", "Athletic Club", "Celta Vigo", "Malaga", "Espanyol", "Rayo Vallecano", "Real Sociedad",
+				 "Villareal", "Athletic Bilbao", "Celta Vigo", "Malaga", "Espanyol", "Rayo Vallecano", "Real Sociedad",
 				 "Elche", "Levante", "Getafe", "Deportivo La Coruna", "Granada", "Eibar", "Almeria", "Cordoba",
 				 "Real Betis"]
 
-def getTeams(title):
+def out(message, file):
+	file.write(message.encode('utf8') + '\n')
+	print message
+
+
+def getTeams(title, f):
 	teams = title.split("-")
 	spanish_match = True
 	for tm in teams:
 		if tm not in SPANISH_TEAMS:
-			print tm + " not Spanish.---"
+			#print tm + " not Spanish.---"
+			message = tm + " not Spanish.---"
+			out (message, f)
 			return False;
 	return spanish_match
 
-def checkPenalty(list, type):
+def checkPenalty(list, type, f):
 	contador = 0
 	for el in list:
 		contador += 1
@@ -26,54 +33,68 @@ def checkPenalty(list, type):
 			eventText = el.text.replace('\n',' ')
 			#eventText = el.text.upper()
 			if "PENALTY" in eventText.upper():
-				print "\t" + type + " - " + eventText
+				#print "\t" + type + " - " + eventText
+				message = "\t" + type + " - " + eventText
+				out(message,f)
 		#print str(contador) + " - " + el.text
 
+### ----- Driver setup ------
 driver = webdriver.Firefox()
 driver.maximize_window()
 #driver.set_window_position(-110,-600)
 driver.maximize_window()
 driver.delete_all_cookies()
 
-print ":::OpenPenalty: Start script at::: " + time.ctime()
+with open('outFile.txt', 'w+') as f:
+
+	message = ":::OpenPenalty: Start script at::: " + time.ctime()
+	out (message, f)
+
+	baseURL = "http://www.whoscored.com/Matches/"
 
 
-baseURL = "http://www.whoscored.com/Matches/"
+	#urlList = ["http://www.whoscored.com/Matches/985501/Live", "http://www.whoscored.com/Matches/985519/Live", "http://www.whoscored.com/Matches/985563/Live",
+	#		   "http://www.whoscored.com/Matches/985555/Live", "http://www.whoscored.com/Matches/985553/Live"]
+	urlList = []
+	#url = "http://www.whoscored.com/Matches/985501/Live"
 
+	#build url list
+	i = 862052
+	fin = 862258
+	while i < fin:
+		i += 1
+		url_aux = baseURL + str(i) + "/Live"
+		urlList.append(url_aux)
 
-urlList = ["http://www.whoscored.com/Matches/985501/Live", "http://www.whoscored.com/Matches/985519/Live", "http://www.whoscored.com/Matches/985563/Live",
-		   "http://www.whoscored.com/Matches/985555/Live", "http://www.whoscored.com/Matches/985553/Live"]
+	for url in urlList:
+		driver.get(url)
+		#print "que pasa" + url
 
-#url = "http://www.whoscored.com/Matches/985501/Live"
+		spanishMatch = getTeams(driver.title, f)
 
-for url in urlList:
-	driver.get(url)
-	#print "que pasa" + url
+		message = "# Match: " + driver.title + " | URL: " + url
+		out (message, f)
 
-	spanishMatch = getTeams(driver.title)
+		if spanishMatch:
+			links = driver.find_elements_by_partial_link_text('Match Commentary')
+			for link in links:
+				#print link.get_attribute("href")
+				link.click()
 
-	print "# Match: " + driver.title + " | URL: " + url
+			#print "aqui estamos"
+			total_pages_text = driver.find_element_by_class_name('total-pages')
 
-	if spanishMatch:
-		links = driver.find_elements_by_partial_link_text('Match Commentary')
-		for link in links:
-			#print link.get_attribute("href")
-			link.click()
+			totalPages = int(total_pages_text.text)
+			currentPage = 0
+			while currentPage < totalPages:
+				currentPage += 1
+				list15 = driver.find_elements_by_xpath("//*[@data-type=15]")
+				checkPenalty(list15, "FAIL", f)
+				list16 = driver.find_elements_by_xpath("//*[@data-type=16]")
+				checkPenalty(list16, "GOAL", f)
 
-		#print "aqui estamos"
-		total_pages_text = driver.find_element_by_class_name('total-pages')
-
-		totalPages = int(total_pages_text.text)
-		currentPage = 0
-		while currentPage < totalPages:
-			currentPage += 1
-			list15 = driver.find_elements_by_xpath("//*[@data-type=15]")
-			checkPenalty(list15, "FAIL")
-			list16 = driver.find_elements_by_xpath("//*[@data-type=16]")
-			checkPenalty(list16, "GOAL")
-
-			#print "Next page"
-			spans = driver.find_elements_by_class_name('page-navigation-item')
-			spans[2].click()
+				#print "Next page"
+				spans = driver.find_elements_by_class_name('page-navigation-item')
+				spans[2].click()
 
 print "end script"
